d751de816125c79b66d433b6914d94cc4fd56c06
NgocThang <pnthangkth@gmail.com>
2026-05-13 13:04:46 +0700

    update evaluation

--- src/search_tfidf/__init__.py
+++ src/search_tfidf/__init__.py
@@ -1,12 +1,16 @@
 from .documents import NewsDocument, SearchResult
+from .bm25_engine import BM25SearchEngine
 from .engine import TfidfSearchEngine
 from .io import read_jsonl, write_jsonl
+from .text_utils import normalize_text, tokenize_text
 
 __all__ = [
+    "BM25SearchEngine",
     "NewsDocument",
     "SearchResult",
     "TfidfSearchEngine",
+    "normalize_text",
     "read_jsonl",
+    "tokenize_text",
     "write_jsonl",
 ]
-
